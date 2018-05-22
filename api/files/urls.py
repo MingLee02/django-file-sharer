3349b61f6f97442308121a7f2285fae0219c3c45
ming lee <ming.lee@hotmail.com>
2018-05-22 20:38:51 +0100

    Add a file list view.

--- api/files/urls.py
+++ api/files/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import FileView
+from .views import FileView, FileList
 
 
 urlpatterns = [
   url(r'^upload/$', FileView.as_view(), name='file-upload'),
+  url(r'^list/$', FileList.as_view(), name='file-list'), 
 ]
--- api/files/views.py
+++ api/files/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import FileSerializer
+from .models import File
 
 
 class FileView(APIView):
@@ -15,3 +17,8 @@ class FileView(APIView):
             return Response(file_serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FileList(generics.ListAPIView):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
