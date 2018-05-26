00a66106b68b616f76e2c7f1daf5458ccd6a7b9b
ming lee <ming.lee@hotmail.com>
2018-05-26 11:28:15 +0100

    Add migration and display tile instead of object object.

--- api/files/models.py
+++ api/files/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 
 class File(models.Model):
-	file = models.FileField(blank=False, null=False)
-	description = models.TextField(null=True, blank=True)
-	title = models.CharField(max_length=255, null=True, blank=True)
-	timestamp = models.DateTimeField(auto_now_add=True)
-	is_public = models.BooleanField(default=False)
-	owner = models.ForeignKey('auth.User', related_name='files', on_delete=models.CASCADE)
+    file = models.FileField(blank=False, null=False)
+    description = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_public = models.BooleanField(default=False)
+    owner = models.ForeignKey('auth.User', related_name='files', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
